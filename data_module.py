1871a78d28e38bb08e937c53583e5900e8c9fc91
identity-fraud <william.kim22@education.nsw.gov.au>
2025-08-12 11:10:22 +1000

    Update data_module.py

--- data_module.py
+++ data_module.py
@@ -1,16 +1,20 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
-def internet_users_line():
-    internet_users = pd.read_csv('Data/number-of-internet-users.csv') 
+def internet_users_graph(kind):
+    internet_users = pd.read_csv('Data/percent-of-internet-users.csv') 
     internet_users.plot(
-                kind='line',
+                kind=kind,
                 x='Year',
                 y='Individuals using the Internet in Australia (%)',
                 color='blue',
                 alpha=0.3,
-                title='Number of Internet users in Australia every year'
-                   )
+                title='Number of Internet users in Australia every year',
+                yticks=(0,5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100),
+                xticks =(1990, 1995, 2000, 2005, 2010, 2015, 2020, 2025)
+                    )
 
     plt.show()
+
+
+mental_disorders = pd.read_csv('Data/percent-of-mental-disorders.csv')
