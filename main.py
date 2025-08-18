fe02de389c95529266439d98b762016ec43ab253
identity-fraud <william.kim22@education.nsw.gov.au>
2025-08-18 12:30:35 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-from data_module import internet_users_graph
+from data_module import internet_users_graph, mental_disorders_graph
+import time
+# kind can be: line, scatter
 
-internet_users_graph(line)
+def main():
+        print("Interface\n")
+        print("1. View percent of people using the internet in Australia visualisation")
+        print("2. View percent of people with mental disorders in Australia visualisation")       
+        print("3. View percent of people using the internet in Australia dataset")
+        print("4. View percent of people with mental disorders in Australia dataset")
+        print("5. Exit") 
+
+        interface_choice = input()
+        
+if __name__ == "__main__":
+    main()
