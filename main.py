44bf4a54ccdd9081131595fb548f454e107e3d2b
transaction-fraud <william.kim22@education.nsw.gov.au>
2025-08-21 17:10:16 +1000

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,21 @@ import time
 
 def main():
         while True:
-                time.sleep(1)
-                print("Interface\n")
+                print(" -- Interface -- ")
                 print("1. View percent of people using the internet in Australia visualisation")
                 print("2. View percent of people with mental disorders in Australia visualisation")       
                 print("3. View percent of people using the internet in Australia dataset")
                 print("4. View percent of people with mental disorders in Australia dataset")
                 print("5. Exit") 
+                print(" -- Interface -- ")
 
                 interface_choice = input('Select choice: ').strip()
 
                 if interface_choice == '1':
                 
                         try:
-                                kind = input('Select graph type (scatter, line): ').strip()
+                                kind = input('Select graph type (scatter, line): ').strip().lower()
+                                print('Close graph to continue')
                                 internet_users_graph(kind)
 
                         except ValueError:
@@ -27,20 +28,22 @@ def main():
 
                 elif interface_choice == '2':
                         try:
-                                kind = input('Select graph type (scatter, line): ').strip()
+                                kind = input('Select graph type (scatter, line): ').strip().lower()
+                                print('Close graph to continue')
                                 mental_disorders_graph(kind)
 
                         except ValueError:
                                 print('Incorrect input, select from scatter or line')
                 
                 elif interface_choice == '3':
-
-                        internet_users_dataset(None)
-                        index = input('Filter through dataset by years')
-                        internet_users_dataset(index)
+                        year = input('Filter through dataset by years (Press enter to output full dataset): ')
+                        internet_users_dataset(year)
+                        input('Press enter to continue: ')
                 
                 elif interface_choice == '4':
-                        mental_disorders_dataset()
+                        year = input('Filter through dataset by years (Press enter to output full dataset): ')
+                        mental_disorders_dataset(year)
+                        input('Press enter to continue: ')
 
                 elif interface_choice == '5':
                         break
